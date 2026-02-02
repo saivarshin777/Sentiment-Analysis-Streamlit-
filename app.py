735f48c8ad7df1fde4cf94a80c55f4210a42c7d9
P.Sai Varshini <laksh.br9@gmail.com>
2026-02-02 20:38:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-# Download resources (only first time)
+import pickle
+
 nltk.download('stopwords')
 nltk.download('wordnet')
 
-# Load model and vectorizer
 model = pickle.load(open("imdb_sentiment_model.pkl", "rb"))
-vectorizer = pickle.load(open("imdb_tfidf_vectorizer.pkl
+vectorizer = pickle.load(open("imdb_tfidf_vectorizer.pkl", "rb"))
+
+
